d3ae07fd0742eb7b2749a757671f8c6ca5629bde
rustiageena <geenarustia@yahoo.com>
2020-10-28 11:47:23 -0700

    Update HShape.java

--- RecursiveDrawings/src/main/java/recur/HShape.java
+++ RecursiveDrawings/src/main/java/recur/HShape.java
@@ -3,10 +3,29 @@ package recur;
 import java.awt.Color;
 
 public class HShape extends AbstractShape {
-
-    public HShape() {
-        
-        
-    }
     
-}
+    @Override
+    public boolean addLevel() {
+        throw new UnsupportedOperationException("Not supported yet.");
+        //To change body of generated methods, choose Tools | Templates. 
+        {
+    @Override
+    public boolean removeLevel() {
+        throw new UnsupportedOperationException("Not supported yet.");
+        //To change body of generated methods, choose Tools | Templates.
+        {
+    @Override
+        public void draw(Graphics2D g) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        //To change body of generated methods, choose Tools | Templates.
+        {  
+     @Override
+        public int countShapes() {
+            throw new UnsupportedOperationException("Not supported yet.");
+        //To change body of generated methods, choose Tools | Templates.
+        {   
+      @Override
+        public void update(int value) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        //To change body of generated methods, choose Tools | Templates.
+       {
