33d8c19d390b8cd7d12f001bd52b7fc3177311f5
Tram Nguyen <72321705+tramnguyen1127@users.noreply.github.com>
2020-10-28 11:55:17 -0700

    Update HShape.java

--- RecursiveDrawings/src/main/java/recur/HShape.java
+++ RecursiveDrawings/src/main/java/recur/HShape.java
@@ -4,28 +4,24 @@ import java.awt.Color;
 
 public class HShape extends AbstractShape {
     
-    @Override
-    public boolean addLevel() {
+    public HShape() {
+        super();
+    }
+    
+   
+    
+    public void draw(Graphics2D g) {
+         throw new UnsupportedOperationException("Not supported yet.");
+        //To change body of generated methods, choose Tools | Templates.
+        }  
+    
+ 
+    public void update(int value) {
         throw new UnsupportedOperationException("Not supported yet.");
-        //To change body of generated methods, choose Tools | Templates. 
-        {
-    @Override
-    public boolean removeLevel() {
-        throw new UnsupportedOperationException("Not supported yet.");
         //To change body of generated methods, choose Tools | Templates.
-        {
-    @Override
-        public void draw(Graphics2D g) {
-            throw new UnsupportedOperationException("Not supported yet.");
-        //To change body of generated methods, choose Tools | Templates.
-        {  
-     @Override
-        public int countShapes() {
-            throw new UnsupportedOperationException("Not supported yet.");
-        //To change body of generated methods, choose Tools | Templates.
-        {   
-      @Override
-        public void update(int value) {
-            throw new UnsupportedOperationException("Not supported yet.");
-        //To change body of generated methods, choose Tools | Templates.
-       {
+    }
+    
+    public void createChildren() {
+    }
+}
+           
