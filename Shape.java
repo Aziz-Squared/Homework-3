568fc64f7bab947899d556f76d54927a063b8167
Aziz <aziz98366@gmail.com>
2020-10-31 00:47:13 -0700

    Commiting

--- Shape.java
+++ Shape.java
@@ -6,16 +6,14 @@ import java.awt.Point;
 
 public interface Shape {
 
-    void draw(Graphics g); // Draws this shape
-	
-	public boolean addLevel(); // Adds a level to this shape. Returns true if the operation was
-						       // successful or false if the maximum level has been reached.
-	public boolean removeLevel(); // Removes a level from this shape. Returns true if the 
-								  // operation was successful or false if the shape was at level 1.
-	public int countShapes(); //Returns the total number of shapes of this shape 
-							  // (e.g. 57 for the H shape at level 2).
-	public void update(int value); // Modifies this shape in an interesting way given a value in 
-								   // the range 0-100. 
+	void draw(Graphics g);
 
-    
+	public boolean addLevel();
+
+	public boolean removeLevel();
+
+	public int countShapes();
+
+	public void update(int value);
+
 }
