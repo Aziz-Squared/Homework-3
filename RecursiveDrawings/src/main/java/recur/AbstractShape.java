0a422797ac2f04a7e0aee4b24e6bdbd532cce9fd
Aziz <aziz98366@gmail.com>
2020-10-31 16:19:10 -0700

    Staging commit

--- RecursiveDrawings/src/main/java/recur/AbstractShape.java
+++ RecursiveDrawings/src/main/java/recur/AbstractShape.java
@@ -1,11 +1,11 @@
 package recur;
 
-import java.awt.Graphics;
 import java.awt.Color;
 
 public abstract class AbstractShape implements Shape {
 
     protected static int level = 0;
+    protected static int count = 0;
     protected int maxLevel;
     protected AbstractShape[] children;
     protected Color color;
@@ -20,6 +20,7 @@ public abstract class AbstractShape implements Shape {
     public boolean addLevel() {
 
         if (children[0] == null) {
+            System.out.println("Current level: " + level);
             if (level <= maxLevel) {
                 createChildren();
                 return true;
@@ -27,10 +28,8 @@ public abstract class AbstractShape implements Shape {
                 return false;
             }
         } else {
-            // level++;
             for (int i = 0; i <= children.length - 1; i++) {
                 children[i].addLevel();
-
             }
             return true;
         }
@@ -42,11 +41,14 @@ public abstract class AbstractShape implements Shape {
         // base case is if there are no grandchildren
         if (children[0] != null && children[0].children[0] == null) {
             if (children[0].children[0] == null) {
-                children[0] = null;
+                for (int i = 0; i <= children.length - 1;) {
+                    children[i] = null;
+                    return true;
+                }
                 return true;
             } else {
-                for (int i = 0; i < children.length; i++) {
-                    children[0].removeLevel();
+                for (int i = 0; i <= children.length - 1; i++) {
+                    children[i].removeLevel();
                 }
                 return true;
             }
@@ -57,11 +59,10 @@ public abstract class AbstractShape implements Shape {
 
     @Override
     public int countShapes() {
-        int count = 0;
         if (children[0] == null) {
             return 1;
         } else {
-            for (int i = 0; i < children.length; i++) {
+            for (int i = 0; i <= children.length - 1; i++) {
                 count = 1 + children[i].countShapes();
             }
         }
