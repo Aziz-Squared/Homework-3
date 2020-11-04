d5491a6aba40dd53e39bd50472930297c647bdf3
Aziz <aziz98366@gmail.com>
2020-11-03 17:14:04 -0800

    Upload

--- RecursiveDrawings/src/main/java/recur/HShape.java
+++ RecursiveDrawings/src/main/java/recur/HShape.java
@@ -14,7 +14,7 @@ public class HShape extends AbstractShape {
 
         HEIGHT = height;
         WIDTH = width;
-        points = newRectangles(new Point(WIDTH - WIDTH, HEIGHT - HEIGHT), new Point(WIDTH, HEIGHT));
+        points = newRectangles(new Point(0, 0), new Point(WIDTH, HEIGHT));
 
     }
 
@@ -37,23 +37,24 @@ public class HShape extends AbstractShape {
                 for (int i = 0; i <= children.length - 1; i++) {
                     for (int j = 1; j <= 17; j += 2) {
                         children[i] = new HShape(
-                            new Point(childWidth * col + points[j - 1].x, childHeight * row + points[j - 1].y),
-                            new Point(childWidth * col + points[j].x, childHeight * row + points[j].y) );
+                                new Point(childWidth * col + points[j - 1].x, childHeight * row + points[j - 1].y),
+                                new Point(childWidth * col + points[j].x, childHeight * row + points[j].y));
 
                     }
 
                 }
             }
         }
-
-        // for (int i = 0; i <= children.length - 1; i++) {
-        // for (int j = 1; j <= 17; j += 2) {
-        // children[i] = new HShape(points[j - 1], points[j]);
-
-        // }
-        // }
-
     }
+
+    // for (int i = 0; i <= children.length - 1; i++) {
+    // for (int j = 1; j <= 17; j += 2) {
+    // children[i] = new HShape(points[j - 1], points[j]);
+
+    // }
+    // }
+
+    // }
 
     @Override
     public void draw(Graphics g) {
@@ -65,7 +66,7 @@ public class HShape extends AbstractShape {
             for (int i = 1; i <= 17; i += 2) {
                 // Checks to see if the starting points are the two white squares
                 if ((i - 1) == 6 || (i - 1) == 10) {
-                    g.setColor(Color.WHITE);
+                    g.setColor(Color.BLACK);
                     g.fillRect(points[i - 1].x, points[i - 1].y, points[i].x, points[i].y);
                 } else {
                     g.setColor(Color.GREEN);
@@ -88,22 +89,24 @@ public class HShape extends AbstractShape {
     // Create the points of the rectangles
     public Point[] newRectangles(Point newLimit, Point newLimit2) {
 
-        WIDTH = newLimit2.x - newLimit.x;
-        HEIGHT = newLimit2.y - newLimit.y;
-        int widthZero = WIDTH - WIDTH;
-        int heightZero = HEIGHT - HEIGHT;
-        int widthThird = WIDTH / 3;
-        int heightThird = HEIGHT / 3;
+        int widthZero = newLimit.x;
+        int heightZero = newLimit.y;
+
+        int widthMax = newLimit2.x;
+        int heightMax = newLimit2.y;
+
+        int widthThird = newLimit2.x / 3;
+        int heightThird = newLimit2.y / 3;
 
         return new Point[] { new Point(widthZero, heightZero), new Point(widthThird, heightThird), // Rectangle 1
                 new Point(widthZero, heightThird), new Point(widthThird, heightThird * 2), // Rectangle 2
-                new Point(widthZero, heightThird * 2), new Point(widthThird, HEIGHT), // Rectangle 3
+                new Point(widthZero, heightThird * 2), new Point(widthThird, heightMax), // Rectangle 3
                 new Point(widthThird, heightZero), new Point(widthThird * 2, heightThird), // Rectangle 4
                 new Point(widthThird, heightThird), new Point(widthThird * 2, heightThird * 2), // Rectangle 5
-                new Point(widthThird, heightThird * 2), new Point(widthThird * 2, HEIGHT), // Rectangle 6
-                new Point(widthThird * 2, heightZero), new Point(WIDTH, heightThird), // Rectangle 7
-                new Point(widthThird * 2, heightThird), new Point(WIDTH, heightThird * 2), // Rectangle 8
-                new Point(widthThird * 2, heightThird * 2), new Point(WIDTH, HEIGHT) }; // Rectangle 9
+                new Point(widthThird, heightThird * 2), new Point(widthThird * 2, heightMax), // Rectangle 6
+                new Point(widthThird * 2, heightZero), new Point(widthMax, heightThird), // Rectangle 7
+                new Point(widthThird * 2, heightThird), new Point(widthMax, heightThird * 2), // Rectangle 8
+                new Point(widthThird * 2, heightThird * 2), new Point(widthMax, heightMax) }; // Rectangle 9
 
     }
 
